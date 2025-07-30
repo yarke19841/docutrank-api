81e8a24b28e262885b6d56554a1a01ef84f82175
yarke19841 <42985423+yarke19841@users.noreply.github.com>
2025-07-30 05:00:30 -0500

    Fix: ajustes en rutas y config para despliegue en Render

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+        return response()->json(['message' => 'API Laravel funcionando']);
+
 });
 
 
